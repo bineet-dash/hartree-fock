3e8e7d7e210b9ecaba342a76e3f7e99b033b9caa
Bineet Dash <bineetdash@gmail.com>
2017-11-03 21:00:41 +0530

    take action based on c++ exit codes (eg. record skipped pts for code 9)

--- runfile.sh
+++ runfile.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-for (( separation = 35; separation <= 36; separation++ ))
+for (( separation = 94; separation <= 95; separation++ ))
 do
-   ./HF $separation
-   echo "program completed for separation=".$separation
+  ./HF-gaussian $separation 5; result=$?
+  if [ $result -eq 9 ];
+  then
+    echo "Too many loops, program skipped for separation=".$separation
+    echo $separation >> data/skipped_points.txt
+  elif [ $result -eq 0 ];
+   then
+    echo "program completed successfully for separation=".$separation
+  fi
+
 done
 
 newfilename="data/energy_correction_data "$(date)".txt"
-
 mv data/correction.txt "$newfilename"
+
+$newfilename="data/skipped_points "$(date)".txt"
+mv data/skipped_points.txt "$newfilename"
