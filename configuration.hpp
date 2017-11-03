b220dd94d2989fc1238fdf73237ffcf9c57c5055
Bineet Dash <bineetdash@gmail.com>
2017-11-03 17:22:30 +0530

    variable explanations added.

--- configuration.hpp
+++ configuration.hpp
@@ -3,17 +3,28 @@
 
 using namespace std;
 
-int no_of_pts=1000;
-double low_lim = -6.0;
-double up_lim = 6.0;
-double dx = (up_lim - low_lim)/double(no_of_pts);
-const int no_of_sps = 3;
-double tolerance = 0.0001;
+/*Declaration of parameters fixed during runtime. */
 
-const double omega=1.0;
+/*Essential parameters.*/
+/*Don't add your own parameter in this block */
+
+int no_of_pts=1000;       //Number of grid points
+double low_lim = -6.0;    //Lower limit of x. Grid starts here
+double up_lim = 6.0;      //Upper Limit of x. Grid ends here.
+double dx = (up_lim - low_lim)/double(no_of_pts); //Step size
+const int no_of_sps = 3;  //Number of electrons (i.e. no of trial basis statesa)
+double tolerance = 1e-4;  //Tolerance for the convergence of hartree_fock iterations
 
 double V(double);
+/*Essential parameters end*/
 
+/*Custom parameters*/
+/*Define your own parameters here*/
+const double omega=1.0;
+
+/*Custom parameters end*/
+
+/*Definition of Potential*/
 double V(double x)
 {
   return 0.5*pow(omega*x,2);
